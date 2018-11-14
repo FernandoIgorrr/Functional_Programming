2869799f1e36ceebdff3058705cd99dee240f374
FernandoIgor <fernandoigor.dantas@outlook.com>
2018-11-14 18:19:03 -0200

    add Sh ow and Eq

--- ExNat.hs
+++ ExNat.hs
@@ -1,5 +1,18 @@
 module ExNat where
 
+import Prelude ( Show(..), Eq(..), Bool(..), (++) )
+
 data Nat = Zero | Succ Nat
 
+instance Show Nat where
+
+ show Zero     = "0"
+ show (Succ m) = "S" ++ show m
+
+instance Eq Nat where
+
+ Zero     == Zero     = True
+ Zero     == (Succ _) = False
+ (Succ _) == Zero     = False
+ (Succ m) == (Succ n) = m == n
 
