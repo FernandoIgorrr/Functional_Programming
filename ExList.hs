427af3e2610dc7c88e28d75f6dd126bf89656954
FernandoIgorrr <FernandoIgor.Dantas@outlook.com>
2019-03-26 18:18:58 -0300

    Adding more complex functions involving 2-tuples

--- ExList.hs
+++ ExList.hs
@@ -14,8 +14,30 @@ import qualified Data.Char as C
 
 myFactorial :: Integer -> Integer
 myFactorial 0	= 1
-myFactorial n	|n < 0		= error "Sub zero"
-		|otherwise	= n * myFactorial (n - 1)
+myFactorial n
+	|n < 0		= error "Sub zero"
+	|otherwise	= n * myFactorial (n - 1)
+
+myFactorial' :: Integer -> Integer
+myFactorial' 0	= 1
+myFactorial' n 
+	|n < 0		= error "Sub zero"
+	|otherwise	= myProd [1..n]
+
+repeatChar :: Int -> [Char] -> [Char]
+repeatChar 0 _	= []
+repeatChar _ []	= []
+repeatChar n xs
+	| n < 0		= error "Sub zero"
+	| otherwise	= xs ++ repeatChar (n-1) xs
+
+rc :: [Char] -> Int -> [Char]
+rc [] _	= []
+rc _ 0	= []
+rc xs n
+	| n < 0		= error "Sub zero"
+	| otherwise	= xs ++ rc xs (n-1)
+
 
 myHead :: [a] -> a
 myHead []	= error "Empty List"
@@ -67,15 +89,17 @@ myDrop n (x:xs)	= myDrop (n -1) xs
 myMaximum :: Ord a => [a] -> a
 myMaximum [] 	= error "Empty List"
 myMaximum [x]	= x
-myMaximum (x:xs)|x > max	= x
-		|otherwise	= max
+myMaximum (x:xs)
+	|x > max	= x
+	|otherwise	= max
 	where max = myMaximum xs 
 
 myMinimum :: Ord a => [a] -> a
 myMinimum []	= error "Empety List"
 myMinimum [x]	= x
-myMinimum (x:xs)|x < min	= x
-		|otherwise	= min
+myMinimum (x:xs)
+	|x < min	= x
+	|otherwise	= min
 	where min = myMinimum xs
 
 mySum :: Num a => [a] -> a
@@ -99,6 +123,24 @@ myCycle xs = xs ++ myCycle xs
 myRepeat :: a -> [a]
 myRepeat n = n : myRepeat n 
 
+myBreak :: (a -> Bool) -> [a] -> ([a],[a])
+myBreak _ []		= ([],[])
+myBreak p xxs@(x:xs)
+	|p x		= ([],xxs)
+	|otherwise	= (x:ys,zs)
+		where (ys,zs) = myBreak p xs
+
+myLines :: String -> [String]
+myLines []	= []
+myLines s	= [s]
+
+myWords :: String -> [String]
+myWords []		= []
+myWords xxs@(x:xs)
+	| x == ' '	= myWords xs
+	| otherwise	= ys:myWords rest
+		where (ys, rest) = myBreak (== ' ') xxs
+
 myFst :: (a,b) -> a
 myFst (x,_)	= x
 
@@ -111,15 +153,23 @@ myZip []	_	= []
 myZip (x:xs)	(y:ys)	= (x,y) : myZip xs ys
 
 myUnzip :: [(a,b)] -> ([a],[b])
-myUnzip []		= error "Empty list"
-myUnzip [(x,y)] 	= ([x],[y])
-myUnzip ((x,y):xs)	= (x: myFst l, y: mySnd l)
+myUnzip []		= ([],[])
+myUnzip ((x,y):xys)	= (x:l, y:r)
 	where
-	l = myUnzip xs
+	l = myFst $ myUnzip xys
+	r = mySnd $ myUnzip xys
+
+myUnzip' :: [(a,b)] -> ([a],[b])
+myUnzip' []		= ([],[])
+myUnzip' ((x,y):xys)	= (x:myFst (myUnzip' xys), y:mySnd (myUnzip' xys))
 
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
 myFoldr f z []		= z
 myFoldr f z (x:xs)	= f x (myFoldr f z xs) 
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f z []		= z
+myFoldl f z (x:xs)	= myFoldl f (f z x) xs
 
 (++) :: [a] -> [a] -> [a]
 []	++ ys	= ys
